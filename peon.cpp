30b88a288ff34442ddb3a8c220be147ce9a158dc
Carlos Eduardo <carlos.0577@outlook.com>
2023-05-20 16:35:29 -0600

    :sparkles: Validar movimientos del peon

--- peon.cpp
+++ peon.cpp
@@ -20,9 +20,43 @@ bool Peon::validarMovimiento(int fila, int columna, Pieza* casilla){
 		return false;
 	}
 
+	//El peon aun no se ha movido
 	if(primerMovimiento){
-		if((m_columna==columna) && ()){
-
+		//Quiere moverse dos casillas y el destino no esta ocupado
+		if((m_columna==columna) && (m_fila-fila)==(m_color=='B'? -2 : 2) && casilla==nullptr){
+			cout<<"Movimiento VALIDO."<<endl;
+			primerMovimiento=false;
+			return true;
 		}
 	}
+
+	//Se quiere mover mas de 1 casilla o de lado (Moverse, No Comer)
+	if(((m_fila-fila)!=(m_color=='B'? -1 : 1)) || abs(m_columna-columna)>1){
+		cout<<"Movimiento NO VALIDO: El peon no puede moverse a esa posicion."
+		return false;
+	}
+
+	//Se quiere mover de lado
+	if(abs(m_columna-columna)){
+		//Casilla destino sola (No puede comer)
+		if(casilla==nullptr){
+			cout<<endl<<"Movimiento No VALIDO: Para comer, la casilla debe tener una pieza "<<(m_color=='B'? "NEGRA" : "BLANCA");
+			return false;
+		}
+
+		//Casilla destino ocupada por una pieza propia
+		if(casilla->getColor()==m_color){
+			cout<<endl<<"Movimiento NO VALIDO: La casilla debe tener una pieza "<<(color=='B'? "NEGRA" : "BLANCA");
+			return false;
+		}
+		
+	}else if(casilla!=nullptr){ //Quiere moverse al frente (comiendo)
+		cout<<"Movimiento NO VALIDO: La casilla debe estar vacia."<<endl;
+		return false;		
+	}
+
+	//Para los demas casos:
+	cout<<"Movimiento VALIDO."<<endl;
+	primerMovimiento=false;
+	return true;
 }
