ed9a8b07fd8f86292b78b88840d324ec654f8391
Carlos Eduardo <carlos.0577@outlook.com>
2023-05-20 23:51:11 -0600

    :sparkles: Implementando todo en clase Partida

---
+++ partida.hpp
@@ -0,0 +1,20 @@
+#ifndef PARTIDA_HPP
+#define PARTIDA_HPP
+
+#include "tablero.hpp"
+
+struct Partida{
+    Partida();
+    
+    void anunciarGanador() const;
+    void realizarMovimiento();
+    void inciarPartida();
+
+    Tablero m_tablero;
+    int opcion;
+    string casillaOrigen;
+    string casillaDestino;
+    bool turno; //  true:Blancas, false:Negras
+};
+
+#endif
